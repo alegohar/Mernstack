e0fd00df7c5c08d7a1b48db1b6edf3d20fb98de6
alegohar <102521210+alegohar@users.noreply.github.com>
2023-11-10 19:02:15 +0500

    Add files via upload
    circles

--- App.jsx
+++ App.jsx
@@ -2,44 +2,44 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Circle from './circle'
+
 
 function App() {
   const [count, setCount] = useState(0)
-  const [date, setDate] = useState(new Date())
-  const [clickHistory, newClickHistory] = useState([])
   return <>
-      <button onClick={() => {
-            setCount( count+1 );
-            const oldHistory = [...clickHistory];
-            oldHistory.push({date: new Date(), counter: count});
-            newClickHistory (oldHistory);
-            }
-          }
-      >Count</button>
-    <button onClick={() => {
-            setCount( 0 );
-            newClickHistory (0);
-          }
-        
-      }>Reset Button</button>
+  <div className='flex gap-20 justify-center overflow-auto'>
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  <Circle/>
 
-{clickHistory.map( function (historyItem)
-{
-  return (
-<div className="numScreen" >  
-    <div>Click Count: {historyItem.counter}</div>
-    <div> Click Time: {historyItem.date.getHours()},{historyItem.date.getMinutes()},{historyItem.date.getSeconds()},
-    </div>
-    </div>
-  )
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  <Circle/>
 
-  
-})
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  <Circle/>
 
-}
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  <Circle/>
+  </div>
+  </>
+    
  
-   </>
-  
 }
 
 export default App
---
+++ Circle.jsx
@@ -0,0 +1,8 @@
+function Circle()
+{
+return (<div className='bg-red-500 p-10 rounded-full w-{400px} h-{400px}'/>)
+      
+    
+  }
+
+  export default Circle;
